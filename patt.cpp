61f5d5326069dbb380f4f07ffc01c79144afc2c2
RohitKaushal7 <rohitkk252550@gmail.com>
2018-10-01 22:03:34 +0530

    changed numbers to * and +

--- patt.cpp
+++ patt.cpp
@@ -3,20 +3,21 @@
 using namespace std;
 int main()
 {
+
 int i,j,k,l;
 for(i=6;i>=1;i--)
 {
 for(j=1;j<i;j++)
 {
-cout<<"*";
+cout<<"  ";
 }
 for(k=6;k>=i;k--)
 {
-cout<<k;
+cout<<"*"<<" ";
 }
 for(l=i+1;l<=6;l++)
 {
-cout<<l;
+cout<<"+"<<" ";
 }
 cout<<endl;
 }
